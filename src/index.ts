18ca367113ee5e67d83fd76471b52804c455b33d
Carlos Vinicius <viniprograming@gmail.com>
2021-01-18 07:55:00 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata'
+import express from 'express'
+import routes from './routes'
+import "./database/connect"
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333, () => console.log("Ta rodando"))
